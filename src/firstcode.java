123ac65b584d6f71727216f0508fc4413286a8a2
JosephMutua <josephmutua1738@gmail.com>
2022-05-10 22:53:38 +0300

    variables

--- src/firstcode.java
+++ src/firstcode.java
@@ -2,7 +2,23 @@
 public class firstcode {
 	
 	public static void main(String[] arg) {
-		System.out.print("Hello World!!!");
+		System.out.println("Hello World!!!");
+		
+		int num_1 = 3; // 4 bytes
+		short num_2 = 4; // 2 bytes
+		byte num_3 = 6; // 1 byte
+		float f1 = 3.4f; // declaring float
+		long l1 = 1000000l; // declaring long variable
+		
+		char a = 'A'; // declaring character variable
+		a= 66; //Overwriting the variable with ASCII converter
+		
+		System.out.println(num_1);
+		System.out.println(num_2);
+		System.out.println(num_3);
+		System.out.println(f1);
+		System.out.println(l1);
+		System.out.println(a);
 		}
 
 }
