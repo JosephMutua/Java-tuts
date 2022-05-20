18e253c9b179c877187fa2f42abf391417049d42
JosephMutua <josephmutua1738@gmail.com>
2022-05-20 16:54:12 +0300

    2 Dimension Arrays

---
+++ src/Arrays2d.java
@@ -0,0 +1,25 @@
+
+public class Arrays2d {
+
+	public static void main(String[] args) {
+		
+		int nums[] [] = {
+				{12,13,14,15},
+				{16,17,14,13},
+				{21,32,25,26}
+		}; 
+		
+		for (int i =0; i < 3;i++) 
+		{
+			
+			for (int j=0;j<4;j++) 
+			{
+				System.out.print(" " + nums [i] [j]);
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}
--- src/ArraysDis.java
+++ src/ArraysDis.java
@@ -3,7 +3,14 @@ public class ArraysDis {
 
 	public static void main(String[] args) {
 		
-		int nums[] = {12,12,34,45,67};
+		/*int nums[] = new int[4];
+		
+		nums [0]=23;
+		nums [1]=24;
+		nums [2]=34;
+		nums [3]=45;
+		*/
+		int nums [] = {12,12,34,45};
 		
 		for (int i=0; i < 4; i++) 
 		{
